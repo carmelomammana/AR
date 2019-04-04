d3fb52c9c4e624a56f448276db4c5bf66f3b43e3
Carmelo Mammana <carmelomammana8@icloud.com>
2019-04-04 18:08:35 +0200

    Game Mode & Edit Mode

---
+++ ARTest/Assets/CubeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public List<GameObject> waypoints;
+    public GameObject cube;
+
+    private float timer;
+    private float timeToMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag("Player").ToList();
+        timeToMove = 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<GameObject> temp = GameObject.FindGameObjectsWithTag("Player").ToList();
+
+        if (waypoints.Count < temp.Count)
+        {
+            waypoints = GameObject.FindGameObjectsWithTag("Player").ToList();
+        }
+        foreach (GameObject waypoint in waypoints)
+        {
+            timer += Time.deltaTime;
+            if (timer > timeToMove)
+            {
+                cube.transform.position = Vector3.MoveTowards(cube.transform.position, waypoint.transform.position, 1.0f);
+                timer = 0;
+            }
+        }
+    }
+}
--- ARTest/Assets/Scripts/ARTapToPlaceObject.cs
+++ ARTest/Assets/Scripts/ARTapToPlaceObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Experimental.XR;
 using UnityEngine.XR.ARFoundation;
 
@@ -8,16 +9,27 @@ public class ARTapToPlaceObject : MonoBehaviour
 {
     public GameObject placementIndicator;
     public GameObject objectToPlace;
+    public GameObject wayText;
+    public GameObject cubeText;
+    public GameObject waypoint;
     
     private ARSessionOrigin arOrigin;
     private Pose placementPose;
     private bool isValidPose;
+    private bool gameMode;
+    private GameObject placement;
+    private float textTime = 5.0f;
+    private float timer;
     
     // Start is called before the first frame update
     void Start()
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
         isValidPose = false;
+        gameMode = false;
+        wayText.SetActive(true);
+        cubeText.SetActive(false);
+        StartCoroutine(TextTime());
     }
 
     // Update is called once per frame
@@ -25,13 +37,51 @@ public class ARTapToPlaceObject : MonoBehaviour
     {
         UpdatePlacementPose();
         UpdatePlacementIndicator();
-
-        if (isValidPose && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        
+        if (isValidPose && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && gameMode)
         {
-            
+            if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                PlaceObject();
+            }
+        }
+
+        if (!gameMode)
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                {
+                    Instantiate(waypoint, placementPose.position, placementPose.rotation);
+                }
+            }
         }
     }
 
+    public void StartGame(bool value)
+    {
+        gameMode = value;
+        StartCoroutine(TextTime());
+    }
+
+    private IEnumerator TextTime()
+    {
+        if(gameMode)
+        {
+            cubeText.SetActive(true);
+            wayText.SetActive(false);
+        }
+        else
+        {
+            wayText.SetActive(true);
+            cubeText.SetActive(false);
+        }
+        yield return new WaitForSeconds(textTime);
+        
+        cubeText.SetActive(false);
+        wayText.SetActive(false);
+    }
+    
     private void PlaceObject()
     {
         Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
@@ -41,12 +91,18 @@ public class ARTapToPlaceObject : MonoBehaviour
     {
         if (isValidPose)
         {
-            placementIndicator.SetActive(true);
-            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            if (placement == null)
+            {
+                placement = Instantiate(placementIndicator, placementPose.position, placementPose.rotation);
+            }
+            Debug.Log(isValidPose + " e true ");
+            placement.SetActive(true);
+            placement.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
         }
         else
         {
-            placementIndicator.SetActive(false);
+            Debug.Log(isValidPose + " e false ");
+            placement.SetActive(false);
         }
     }
     
